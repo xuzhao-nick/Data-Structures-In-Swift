42bc75f8119d9505bd6e4f049e3b24389292c85e
Nick Xu <Nick.Xu@prudential.com.sg>
2020-10-01 15:13:09 +0800

    Add binary tree traverse methods and test cases

--- DataStructures/Sources/DataStructures/BinaryTree.swift
+++ DataStructures/Sources/DataStructures/BinaryTree.swift
@@ -14,3 +14,49 @@ class BinaryNode<Element> {
         self.value = value
     }
 }
+
+extension BinaryNode {
+    func traverseInOrder(visit: (Element) -> Void) {
+        leftChild?.traverseInOrder(visit: visit)
+        visit(value)
+        rightChild?.traverseInOrder(visit: visit)
+    }
+    
+    func traversePreOrder(visit: (Element) -> Void) {
+        visit(value)
+        leftChild?.traversePreOrder(visit: visit)
+        rightChild?.traversePreOrder(visit: visit)
+    }
+    
+    func traversePostOrder(visit: (Element) -> Void) {
+        leftChild?.traversePostOrder(visit: visit)
+        rightChild?.traversePostOrder(visit: visit)
+        visit(value)
+    }
+}
+
+
+extension BinaryNode: CustomStringConvertible {
+  //Note: This algorithm is based on an implementation by Károly Lőrentey in his book Optimizing Collections, available from https://www.objc.io/books/optimizing-collections/.
+
+  public var description: String {
+    return diagram(for: self)
+  }
+
+  private func diagram(for node: BinaryNode?,
+                       _ top: String = "",
+                       _ root: String = "",
+                       _ bottom: String = "") -> String {
+    guard let node = node else {
+      return root + "nil\n"
+    }
+    if node.leftChild == nil && node.rightChild == nil {
+      return root + "\(node.value)\n"
+    }
+    return diagram(for: node.rightChild,
+                   top + " ", top + "┌──", top + "│ ")
+      + root + "\(node.value)\n"
+      + diagram(for: node.leftChild,
+                bottom + "│ ", bottom + "└──", bottom + " ")
+  }
+}
--- DataStructures/Tests/DataStructuresTests/BinaryTreeTestCase.swift
+++ DataStructures/Tests/DataStructuresTests/BinaryTreeTestCase.swift
@@ -24,5 +24,28 @@ final class BinaryTreeTestCase: XCTestCase {
         nine.leftChild = eight
         
         return seven
+    }()
+    
+    func test_visualizeBinaryTree() {
+        print(tree.description)
     }
+    
+    func test_traverseInOrder() {
+        var testArray: [Int] = []
+        tree.traverseInOrder { testArray.append($0)}
+        XCTAssertEqual(testArray, [0,1,5,7,8,9])
+    }
+    
+    func test_traversePreOrder() {
+        var testArray: [Int] = []
+        tree.traversePreOrder { testArray.append($0)}
+        XCTAssertEqual(testArray, [7,1,0,5,9,8])
+    }
+    
+    func test_traversePostOrder() {
+        var testArray: [Int] = []
+        tree.traversePostOrder { testArray.append($0)}
+        XCTAssertEqual(testArray, [0,5,1,8,9,7])
+    }
+    
 }
